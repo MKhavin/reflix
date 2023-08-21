d6b23e299f59eebb109c2a04eec8af7c07b660f9
MichaelK <havinmichael@gmail.com>
2023-08-21 22:24:56 +0300

    Add log out functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,11 @@ import AppLink from "./util/AppLink";
 import { useLocation } from "react-router-dom";
 
 export default function NavBar({ userId }) {
-  const links = ["films", "rented"];
+  const links = {
+    films: `user/${userId}/films`,
+    "rented films": `user/${userId}/rented`,
+    "log out": "/",
+  };
 
   const currentPath = useLocation().pathname;
 
@@ -15,13 +19,9 @@ export default function NavBar({ userId }) {
   return (
     <nav>
       <ul>
-        {links.map((link, id) => (
-          <AppLink
-            to={`user/${userId}/${link}`}
-            className="navbar-link"
-            key={id}
-          >
-            {link.toUpperCase()}
+        {Object.keys(links).map((key, id) => (
+          <AppLink to={links[key]} className="navbar-link" key={id}>
+            {key.toUpperCase()}
           </AppLink>
         ))}
       </ul>
