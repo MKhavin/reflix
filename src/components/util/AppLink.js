b2f2e7ced9672116738f95227e6e9a52500b59af
MichaelK <havinmichael@gmail.com>
2023-08-21 11:28:35 +0300

    Implement style of AppLink component and ability to render children elements

--- src/components/util/AppLink.js
+++ src/components/util/AppLink.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./css/AppLink.css";
 
-export default function AppLink({ to, children }) {
-  return <Link to={to}>{children}</Link>;
+export default function AppLink({ to, className, children }) {
+  return (
+    <Link to={to} className={`app-link ${className}`}>
+      {children}
+    </Link>
+  );
 }
